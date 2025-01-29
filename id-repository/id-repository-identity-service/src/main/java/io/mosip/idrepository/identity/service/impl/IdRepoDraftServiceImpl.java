f5d8898ee2782fc4b76ea4d4c68f451522a61539
Ashok Kumar Sharma <ashok@mosip.io>
2025-01-29 18:05:23 +0530

    Added logs for debugging the "Missing Input Parameter - identity/IDSchemaVersion" issue.

--- id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoDraftServiceImpl.java
+++ id-repository/id-repository-identity-service/src/main/java/io/mosip/idrepository/identity/service/impl/IdRepoDraftServiceImpl.java
@@ -193,6 +193,13 @@ public class IdRepoDraftServiceImpl extends IdRepoServiceImpl implements IdRepoD
 				newDraft.setCreatedBy(IdRepoSecurityManager.getUser());
 				newDraft.setCreatedDateTime(DateUtils.getUTCCurrentDateTime());
 				uinDraftRepo.save(newDraft);
+				idrepoDraftLogger.info("uinData string from DB : " + new String(newDraft.getUinData()));
+				Optional<UinDraft> uinDraft = uinDraftRepo.findByRegId(registrationId);
+				if (uinDraft.isPresent()) {
+					idrepoDraftLogger.info("uin draft found in DB for RID : " + registrationId);
+					UinDraft savedUinDraft = uinDraft.get();
+					idrepoDraftLogger.info("uinData in uinDraft : " + getUINDataFromDBAsJSONString(savedUinDraft.getUinData()));
+				}
 				idrepoDraftLogger.info("uinData in uinDraft : " + getUINDataFromDBAsJSONString(newDraft.getUinData()));
 				return constructIdResponse(null, DRAFTED, null, null);
 			} else {
